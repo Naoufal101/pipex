780cbdd3b0e4fac76e3e9c39e7b03b429604f110
Naoufal Himad <nhimad@c2r7p1.1337.ma>
2024-05-31 19:49:43 +0100

    Pipex

--- test.c
+++ test.c
@@ -17,10 +17,10 @@ void	ft_free(char **str)
 	}
 	free(str);
 }
-void    leck()
-{
-    system("leaks -q a.out");
-}
+// void    leck()
+// {
+//     system("leaks -q pipex");
+// }
 
 int     main(int argc, char **argv, char **env)
 {
@@ -29,28 +29,31 @@ int     main(int argc, char **argv, char **env)
     char **paths = 0;
     char *valid_path = 0;
     int i = 0;
+    int fd = 0;
 
-    atexit(leck);
-    i = open(argv[1], O_RDONLY);
-    if (i == -1)
-        perror("");
+    fd = open(argv[1], O_RDONLY);
+    if (fd == -1)
+        perror(argv[1]);
+
     else
     {
         dup2(i, 0);
         close(i);
-        i = 0;
+
+        //split here
         cml = ft_split(argv[2], ' ');
         valid_path = cml[0];
-        if (access(valid_path, F_OK) != -1)
+        if (cml[0][0] != '/')
         {
             valid_path = ft_strjoin("/", cml[0]);
             free(cml[0]);
             cml[0] = valid_path;
+
+            //and split here
             paths = ft_split(&env[4][5], ':');
             while(paths[i])
             {
                 valid_path = ft_strjoin(paths[i], cml[0]);
-                printf("here %s\n", valid_path);
                 if (access(valid_path, F_OK) != -1)
                     break ;
                 free(valid_path);
@@ -59,12 +62,12 @@ int     main(int argc, char **argv, char **env)
             if (!paths[i])
                 printf("command not found: %s\n", cml[0]+1);
             ft_free(paths);
+            free(valid_path);
         }
-
-        if (access(valid_path, X_OK) != 1)
+        if (access(valid_path, X_OK) != -1)
             execve(valid_path, cml, env);
         else
-
+            printf("No such file or directory: %s\n", cml[0]);
         ft_free(cml);
     }
 }
